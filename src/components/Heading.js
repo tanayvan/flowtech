3944c3420239fc52347cb6e2155ac9e7b168afd0
Tanay Van <tanayvan258@gmail.com>
2021-06-26 11:11:45 +0530

    changed header animation

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import { useInView } from 'react-intersection-observer'
-import {motion} from "framer-motion"
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 
-export default function Heading({text}) {
+export default function Heading({ text }) {
+  const { inView, ref } = useInView({
+    threshold: 1,
+  });
 
-    const {inView,ref} = useInView({
-        threshold:1
-    })
-
-    return (
-        <div ref={ref} style={{display:"flex",justifyContent:"center",margin:"3rem 0"}}>
-        <motion.div  initial={{width:0}} transition={{duration:0.4}} animate={inView?{width:"auto"}:{width:0}} className="my" style={{display:"flex",justifyContent:"center"}}>
-
-        <div className="text-center category-header" style={{maxLines:1}} >{text}</div>
-        </motion.div>
+  return (
+    <div
+      ref={ref}
+      style={{ display: "flex", justifyContent: "center", margin: "3rem 0" }}
+    >
+      <motion.div
+        initial={{ x: 100, opacity: 0 }}
+        transition={{ duration: 0.4 }}
+        animate={inView ? { x: 0 } : { x: 100, opacity: 0 }}
+        className="my"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <div className="text-center category-header" style={{ maxLines: 1 }}>
+          {text}
         </div>
-    )
+      </motion.div>
+    </div>
+  );
 }
